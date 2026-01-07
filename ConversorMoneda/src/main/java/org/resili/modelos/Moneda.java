3aef26fbd678120e7e8e65ec699e95d39c8cf27a
ResiliopeDAN <danielqqq555@gmail.com>
2026-01-07 12:22:17 -0500

    Agregar modelos de moneda DTO y dominio

---
+++ ConversorMoneda/src/main/java/org/resili/modelos/Moneda.java
@@ -0,0 +1,30 @@
+package org.resili.modelos;
+
+public class Moneda {
+    private final String codigoBase;
+    private final String codigoDestino;
+    private final double tasaDeConversion;
+
+    public Moneda(MonedaDTO monedaDTO) {
+        this.codigoBase = monedaDTO.base_code();
+        this.codigoDestino = monedaDTO.target_code();
+        this.tasaDeConversion = monedaDTO.conversion_rate();
+    }
+
+    public String getCodigoBase() {
+        return codigoBase;
+    }
+
+    public String getCodigoDestino() {
+        return codigoDestino;
+    }
+
+    public double getTasaDeConversion() {
+        return tasaDeConversion;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Tasa de %s a %s: %.4f", codigoBase, codigoDestino, tasaDeConversion);
+    }
+}
